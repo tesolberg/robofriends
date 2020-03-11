5664b4d155c14c31130db4b9147a7567aebce9bd
Tormod Elias Solberg <torelsol@gmail.com>
2020-03-11 21:08:48 +0100

    Updates

---
+++ precache-manifest.96872935a4efc664048033f004533776.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6ca86db261b1096ce490cd43d40b74",
+    "url": "/tesolberg/robofriends/index.html"
+  },
+  {
+    "revision": "2bb990b005e4cf2ed7bc",
+    "url": "/tesolberg/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3e2b81007973130e4bbc",
+    "url": "/tesolberg/robofriends/static/css/main.d0c8466c.chunk.css"
+  },
+  {
+    "revision": "2bb990b005e4cf2ed7bc",
+    "url": "/tesolberg/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tesolberg/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2b81007973130e4bbc",
+    "url": "/tesolberg/robofriends/static/js/main.f2d231b4.chunk.js"
+  },
+  {
+    "revision": "f1777a3420715d289d75",
+    "url": "/tesolberg/robofriends/static/js/runtime-main.0fc2946d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/tesolberg/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
