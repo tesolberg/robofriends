562a0842e1ae679dc1736e7d43385102842dc193
Tormod Elias Solberg <torelsol@gmail.com>
2020-03-11 21:14:19 +0100

    nothing to see here

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll.js";
+import ErrorBoundry from "../components/ErrorBoundry.js";
 
 class App extends Component {
   constructor() {
@@ -37,7 +38,9 @@ class App extends Component {
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChanged={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
